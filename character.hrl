11f861b5dba7169770b39fbf7d23b4b07aa54632
Assemberist <schur.alexandr2015@yandex.ru>
2022-02-03 14:42:11 +0300

    Initial commit
    Created abstract entityes and simple configuration.

---
+++ character.hrl
@@ -0,0 +1,22 @@
+-module(character).
+
+-record (stat, {
+	name		= ""	:: string(),
+	description	= ""	:: string()}).
+
+-record (action, {
+	name		= ""	:: string(),
+	description	= ""	:: string()}).
+
+-record (parameter, {
+	name		= ""	:: string(),
+	value		= 0		:: integer(),
+	description	= ""	:: string()}).
+
+-record(class, {
+	name		= ""	:: string(),
+	parents		= []	:: [string()],
+	actions		= []	:: [string()],
+	stats		= []	:: [string()],
+	parameters	= []	:: [string()],
+	description	= ""	:: string()}).
